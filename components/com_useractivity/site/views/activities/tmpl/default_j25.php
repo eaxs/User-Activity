7c847474c1271ee16e40c00baf9b8f05a7fc9c89
eaxs <tkuhn@live.com>
2013-03-11 14:36:15 +0100

    Added component frontend view for Joomla 2.5

---
+++ components/com_useractivity/site/views/activities/tmpl/default_j25.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @package      User Activity
+ *
+ * @author       Tobias Kuhn (eaxs)
+ * @copyright    Copyright (C) 2013 Tobias Kuhn. All rights reserved.
+ * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
+ */
+
+defined('_JEXEC') or die();
+
+$list_order  = $this->escape($this->state->get('list.ordering'));
+$list_dir    = $this->escape($this->state->get('list.direction'));
+$date_format = $this->params->get('date_format', JText::_('DATE_FORMAT_LC1'));
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_useractivity&view=activities'); ?>" method="post" name="adminForm" id="adminForm" autocomplete="off">
+
+    <!-- Start Filters -->
+    <fieldset class="filters">
+        <?php if ($this->params->get('show_filter_search')) : ?>
+            <input type="text" class="inputbox" placeholder="<?php echo JText::_('COM_USERACTIVITY_FILTER_SEARCH_DESC'); ?>"
+                name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>"
+                title="<?php echo JText::_('COM_USERACTIVITY_FILTER_SEARCH_DESC'); ?>"
+            />
+            <button class="button" type="submit">
+                <?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>
+            </button>
+            <button class="button" type="button" onclick="document.id('filter_search').value='';this.form.submit();">
+                <?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>
+            </button>
+        <?php endif; ?>
+        <?php if ($this->params->get('show_filter_extension')) : ?>
+            <select name="filter_extension" onchange="this.form.submit();" class="inputbox">
+                <option value=""><?php echo JText::_('JOPTION_SELECT_EXTENSION'); ?></option>
+                <?php echo JHtml::_('select.options', $this->extensions, 'value', 'text', $this->state->get('filter.extension')); ?>
+            </select>
+        <?php endif; ?>
+        <?php if ($this->params->get('show_filter_event')) : ?>
+            <select name="filter_event_id" onchange="this.form.submit();" class="inputbox">
+                <option value=""><?php echo JText::_('JOPTION_SELECT_EVENT'); ?></option>
+                <?php echo JHtml::_('select.options', $this->events, 'value', 'text', $this->state->get('filter.event_id')); ?>
+            </select>
+        <?php endif; ?>
+    </fieldset>
+    <!-- End Filters -->
+
+    <!-- Start Items -->
+    <ul>
+        <?php
+        foreach ($this->items as $i => $item) :
+            ?>
+            <li>
+                <span class="row-title"><?php echo $item->text; ?></span>
+                <span class="small">
+                    <?php echo JHtml::_('date', $item->created, $date_format); ?>
+                </span>
+            </li>
+            <?php
+        endforeach;
+        ?>
+    </ul>
+    <!-- End Items -->
+
+    <?php echo $this->pagination->getListFooter(); ?>
+
+    <input type="hidden" name="boxchecked" value="0" />
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="filter_order" value="<?php echo $list_order; ?>" />
+    <input type="hidden" name="filter_order_Dir" value="<?php echo $list_dir; ?>" />
+    <?php echo JHtml::_('form.token'); ?>
+</form>
